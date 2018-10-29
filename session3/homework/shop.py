efba9d56fa46112f623f251f424f947e4c17d4c3
hoangcuong <cuongnh1305.namtien@gmail.com>
2018-10-29 19:02:37 +0700

    initial commit

--- session3/homework/shop.py
+++ session3/homework/shop.py
@@ -1,17 +1,17 @@
 shop_list = ["T-shirt", "Sweater"]
 
 while True:
-    a = (input("Welcome to our shop. What do you want (C, R, U, D)?: "))
+    a = (input("Welcome to our shop. What do you want (C, R, U, D)?: ").upper())
 
-    if a == "C":
-        shop_list.append(input("What item you want to add? "))
-    elif a == "U":
+    if a.upper() == "C":
+        shop_list.append(input("What item you want to add? ").upper())
+    elif a.upper() == "U":
         i = int(input("Which position you want to update? "))
         shop_list[i-1] = input("New item? ")
-    elif a == "D":
+    elif a.upper() == "D":
         n = int(input("What position do you want to delete? "))
         shop_list.pop(n-1)
-    elif a == "R":
+    elif a.upper() == "R":
         pass
     else: 
         print("Please enter the correct (C, R, U, D) ")
